1c630e4968b5e0eb3479ded2fb34ce662da63253
Blakedoghyian <xblakedx@gmail.com>
2022-01-20 16:56:39 -0600

    initial prompt display working

--- server.js
+++ server.js
@@ -21,7 +21,31 @@ app.use((req, res) => {
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
+    promptUser();
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        console.log(`--Server running on port ${PORT}`);
     });
 });
+
+// start inquirer prompt
+
+function promptUser() {
+    inquirer
+    .prompt({
+        name: 'user prompt',
+        type: 'list',
+        message: 'Employee tracker started, please select an option.',
+        choices: [
+
+            'View departments',
+            'View roles',
+            'View employees',
+            'Add department',
+            'Add role',
+            'Add employee',
+            'Update employee',
+            'Delete employee',
+            'Exit'
+        ]
+    })
+}
