87b681b66b96223f3796b5589b9872247d83bfde
Blakedoghyian <91994720+Blakedoghyian@users.noreply.github.com>
2022-01-30 19:48:27 -0600

    removed unnecessary code

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
-const express = require('express');
+
 const inquirer = require('inquirer');
 //require connection creds
 const db = require('./db/connection');
 
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
 
 // Start server after DB connection
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
-    promptUser();
     app.listen(PORT, () => {
         console.log(`--Server running on port ${PORT}`);
     });
@@ -209,3 +197,5 @@ function returnPrompt() {
         }
     })
 };
+
+promptUser();
